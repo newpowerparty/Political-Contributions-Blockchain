088e284dfa5596dd79818e3bde27452c24c1f334
domen111 <domen111@gmail.com>
2017-12-19 22:58:25 +0800

    Show recodes in console Current Smart contract address at ropsten testnet: 0x9b2c9bc786d6c639be126cd3aca00a8d2b927aec

--- Web/index.js
+++ Web/index.js
@@ -1,1 +1,10 @@
-var web3 = new Web3(window.web3.currentProvider);
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/YOUR_ACCESS_TOKEN")); // todo
+}
+var contract = web3.eth.contract([{"constant":false,"inputs":[{"name":"date","type":"uint32"},{"name":"accounts","type":"string"},{"name":"money","type":"bool"},{"name":"amount","type":"uint64"},{"name":"object","type":"string"},{"name":"uniformNumbers","type":"string"},{"name":"receipt","type":"string"},{"name":"abnormal","type":"bool"}],"name":"record","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"date","type":"uint32"},{"indexed":false,"name":"accounts","type":"string"},{"indexed":false,"name":"money","type":"bool"},{"indexed":false,"name":"amount","type":"uint64"},{"indexed":false,"name":"object","type":"string"},{"indexed":false,"name":"uniformNumbers","type":"string"},{"indexed":false,"name":"receipt","type":"string"},{"indexed":false,"name":"abnormal","type":"bool"}],"name":"Record","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}])
+  .at("0x9b2c9bc786d6c639be126cd3aca00a8d2b927aec");
+contract.Record({}, {fromBlock: 0, toBlock: 'latest'}, function(err, res) {
+  console.log(res);
+});
