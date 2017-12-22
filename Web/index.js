55e2749d3aa5f2ce4eeea3a96fa6a74147ebeabe
Domen <domen111@users.noreply.github.com>
2017-12-22 22:01:38 +0800

    Add contractAddress varaible

--- Web/index.js
+++ Web/index.js
@@ -3,8 +3,9 @@ if (typeof web3 !== 'undefined') {
 } else {
   web3 = new Web3(new Web3.providers.HttpProvider("https://api.myetherapi.com/rop"));
 }
+var contractAddress = "0x3d0D9B7b47c8ae90483C82D10f4d630768dA7f48";
 var contract = web3.eth.contract([{"constant":false,"inputs":[{"name":"date","type":"uint32"},{"name":"accounts","type":"string"},{"name":"money","type":"bool"},{"name":"amount","type":"uint64"},{"name":"object","type":"string"},{"name":"uniformNumbers","type":"string"},{"name":"receipt","type":"string"},{"name":"abnormal","type":"bool"}],"name":"record","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"date","type":"uint32"},{"indexed":false,"name":"accounts","type":"string"},{"indexed":false,"name":"money","type":"bool"},{"indexed":false,"name":"amount","type":"uint64"},{"indexed":false,"name":"object","type":"string"},{"indexed":false,"name":"uniformNumbers","type":"string"},{"indexed":false,"name":"receipt","type":"string"},{"indexed":false,"name":"abnormal","type":"bool"}],"name":"Record","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}])
-  .at("0x3d0D9B7b47c8ae90483C82D10f4d630768dA7f48");
+  .at(contractAddress);
 
 // Easier for debugging
 function defaultCallback(err, res) {
